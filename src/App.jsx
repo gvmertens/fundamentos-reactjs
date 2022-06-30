2d7bb1d935da71cf78a69df5ea9e90ad8bb07138
gvmertens <gvmertens@gmail.com>
2022-06-30 16:50:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from './Components/Header'
+import './style.css'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      HELLO GUI
+    </div>
+  )
+}
+
+export default App
