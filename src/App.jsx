ece9aabf568d6f8ac58955f7f43c1419a7ab4a7f
gvmertens <gvmertens@gmail.com>
2022-07-04 16:16:48 -0300

    side

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Header } from './Components/Header'
+import { Sidebar } from './Components/Sidebar'
 import './global.css'
+
+import styles from './App.module.css'
 
 
 function App() {
   return (
     <div>
       <Header />
-      HELLO GUI
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          hello gui
+        </main>
+      </div>
     </div>
   )
 }
